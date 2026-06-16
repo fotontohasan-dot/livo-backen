2e94d0731841650fa2dc995eb58d23decc65ca1b
fotontohasan-dot <fotontohasan@gmail.com>
2026-06-16 15:21:39 +0300

    Refactor message emission logic in socket.js

--- services/socket.js
+++ services/socket.js
@@ -16,22 +16,18 @@ function initSocket(server) {
 
     socket.on('send_message', async (data) => {
       const { senderId, receiverId, message, isAdmin, fileUrl, fileType } = data;
-
       try {
         await pool.query(
           'INSERT INTO chat_messages (sender_id, receiver_id, message, is_admin, file_url, file_type) VALUES ($1, $2, $3, $4, $5, $6)',
           [senderId, receiverId || null, message || '', isAdmin, fileUrl || null, fileType || null]
         );
-
         if (isAdmin) {
           io.to(`user_${receiverId}`).emit('new_message', {
-            senderId, message, isAdmin: true,
-            fileUrl, fileType, createdAt: new Date()
+            senderId, message, isAdmin: true, fileUrl, fileType, createdAt: new Date()
           });
         } else {
           io.to('admin_room').emit('new_message', {
-            senderId, message, isAdmin: false,
-            fileUrl, fileType, createdAt: new Date()
+            senderId, message, isAdmin: false, fileUrl, fileType, createdAt: new Date()
           });
         }
       } catch (err) {
